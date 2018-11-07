78abcbe105bd8358b6d39dd27a96e60b4475327c
Federico Cetraro <cetraro.fede@gmail.com>
2018-11-06 23:11:13 -0300

    combinacion de 2 proyectos

--- Business/MCGA.Business/Properties/AssemblyInfo.cs
+++ Business/MCGA.Business/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
+// set of attributes. Change these attribute values to Edit the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MCGA.Business")]
 [assembly: AssemblyDescription("")]
